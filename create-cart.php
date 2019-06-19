9b9082db9055804467cc6292e6de8636b8e104a4
nebevets <steveben@exmsft.com>
2019-06-19 11:23:44 -0700

    fixed cart endpoint issues

--- create-cart.php
+++ create-cart.php
@@ -1,7 +1,5 @@
 <?php
-// Remember to set created at and updated at to current time
-// Creates a new cart
-// On success, success => true, message => 'Cart created successfully', cart_id => 2
+
   $output = [
     'success' => false
   ];
@@ -16,17 +14,19 @@
   }
   
   if(empty($output['error'])){
-    echo 'connected to database<br>';
-    echo "user_id is $user_id<br>";
     $query = "INSERT INTO `cart` (`user_id`, `created_at`, `updated_at`) VALUES ( $user_id , CURRENT_TIME , CURRENT_TIME );";
-    print($query);
     $result = mysqli_query($conn, $query);
     if($result){
       $output['success'] = true;
       $output['message'] =  'Cart created successfully';
       $output['cart_id'] = mysqli_insert_id($conn);
     } else {
-      $output['error'] = "There was an error performing the query.";
+      $error = mysqli_error($conn);
+      if($error !== ''){
+        $output['error'] = $error;  
+      } else {
+        $output['error'] = "There was an error performing the query.";
+      }
     }
   }
   

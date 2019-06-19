694b81e0fc4cae97b3b6467a01127468d0f6f505
nebevets <steveben@exmsft.com>
2019-06-18 20:53:43 -0700

    modified product list

--- get-product.php
+++ get-product.php
@@ -1,10 +1,14 @@
 <?php
-
   $output = [
     'success' => false
   ];
   $id = null;
   require_once('config.php');
+  if(empty($_GET['id'])){
+    $output['error'] = "You must specify an id.";
+  }else{
+    $id = $_GET['id'];
+  }
   if(empty($output['error'])){
     $query = "SELECT
                 p.id,
@@ -16,23 +20,27 @@
                 i.file_path AS src
               FROM products AS p
               JOIN images AS i
-              ON p.image_id = i.id";
+              ON p.image_id = i.id
+              WHERE p.id = $id";
     $result = mysqli_query($conn, $query);
     if($result){
       $output['success'] = true;
       if(mysqli_num_rows($result)){
-        while($row = mysqli_fetch_assoc($result)){
-          $output['products'][] = [
-            'id' => (int) $row['id'],
-            "name" => $row['name'],
-            "description" => $row['description'],
-            "cost" => (int) $row['cost'],
-            'image' => [
-              'alt' => $row['alt'],
-              'title' => $row['title'],
-              'src' => $row['src'],
-            ]
-          ];
+        $output['products'] = [];
+        if(mysqli_num_rows($result)){
+          while($row = mysqli_fetch_assoc($result)){
+            $output['products'][] = [
+              'id' => (int) $row['id'],
+              "name" => $row['name'],
+              "description" => $row['description'],
+              "cost" => (int) $row['cost'],
+              'image' => [
+                'alt' => $row['alt'],
+                'title' => $row['title'],
+                'src' => $row['src'],
+              ]
+            ];
+          }
         }
       } else {
         $output['products'] = [];
@@ -41,7 +49,6 @@
       $output['error'] = "There was an error performing the query";
     }
   }
-
   print json_encode($output);
  
 ?>
